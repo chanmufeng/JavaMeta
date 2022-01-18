8098d5e2551ce435c2c06d8164b469a40a560a75
chanmufeng <zhaoxaolong@163.com>
2022-01-18 17:17:42 +0800

    修改invoke声明

--- code/designPattern/proxy/dynamicProxy/v3/InvocationHandler.java
+++ code/designPattern/proxy/dynamicProxy/v3/InvocationHandler.java
@@ -7,12 +7,12 @@ public interface InvocationHandler {
     /**
      * 用户自定义逻辑
      *
-     * @param target 执行方法的所属对象
-     * @param m      被执行方法
-     * @param args   被执行方法参数
+     * @param proxy 实际生成的代理对象
+     * @param m     被执行方法
+     * @param args  被执行方法参数
      * @return
      * @throws Throwable
      */
-    Object invoke(Object target, Method m, Object[] args) throws Throwable;
+    Object invoke(Object proxy, Method m, Object[] args) throws Throwable;
 
 }
